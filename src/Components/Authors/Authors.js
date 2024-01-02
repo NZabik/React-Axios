11149702171bd8bf0b97738116df3cdfb5a2182f
Nicolas Zabik <melchiah.nico@gmail.com>
2024-01-02 16:13:03 +0100

    affichage des livres et auteurs en GET

--- src/Components/Authors/Authors.js
+++ src/Components/Authors/Authors.js
@@ -9,7 +9,7 @@ function Author() {
 
     useEffect(() => {
         const headers = {
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MDQyMDI0MjgsImV4cCI6MTcwNDIwNjAyOCwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6ImFkbWluQGJvb2thcGkuY29tIn0.E0JkHQ8Wln3a_w6z2IKk60iN7VyZJERfoXON64Ix2SiADAPeh4li_7iIOK47-ZVTm1WgvQXWX6jJs9RmcPc73k1Uf9QwNfM2lyFqwDsafBqz4XNyFXvGh77HKb5LYkySpCPOKj4Gtix6pDujJ1_sTYKEf12VrwGqMvP5RKMKTvZ_6qfztdu7tud2TnXXAVyN4YaRRPDgf5bITNTPtzhDymAvClSlcyxTRShrzbEPnvnx9R0vSU_nt-L3BMM31Dpxqx-8VXpidEml9dY6bqX__N3TaCpBJ7c7cwYt7bxIw8Lum_QxQvTNlq0FmJGOFBcqkx-vqTnHOJLOtRsLJiST93vZp4eK2FbN5C3BADcAFaylxCVDLpungaKQWpHAUlzvoHgm9IG_SCpMH08m95QMTWW_VAXXr04xYTYVuNRgLXbtkAXdjA2htjqCki-RbUREReaOTb3TcRnlB35ONRx4RusjG1L7Xk9ysyNuGAbSHVH_U5jqUANPOU7DzU0KggJkacd880icxErEw0k9Cp78IEuW_um6VFdG2QKdU6ec9ydkWiW8ZZ8vyTsMYBcOCKMuiIKpuM8uzf8eJM_djcNxqihO1XjKB4H5N1SMOaBOdgzlZOdgUb0VMtrpSsgaFK-XL7hpMVRHZ6QdQeUFZZvscbn1kh3eBvHp7OxYzG9E8FE'
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MDQyMDc5NzIsImV4cCI6MTcwNDIxMTU3Miwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6ImFkbWluQGJvb2thcGkuY29tIn0.I9lVWgzgORd6dUqlLlYX9IEg4O9Ma3Ok2OeLPjT1EEsoAg-kbTKtdBDo0yWsEZ8dFY9oCRrZ4O2_1zrkDfpspFExr8EH0RzKUt2Vdmou8t-lpGhP3JOpw17pvnq05RgrwP8ldBWdkpqe9QoyKpbA4ofHQaMT9jvBOt4b_FJcfqRtWQ6z4fBT9kuansb1U4L-gsXk-GCFZLZX5rY0I2tMZBsZWlo6rLa-vybRsHnrGKq7XK7Y-zz-9gXSZLazV064f9Incv25_m_5kSO3QNoWfSh0-TWwCoPglNesxZ91zaqF0FpQEdbzDE0_Ot9WnLCvUGyFQDlpNiRPq7zwhQe_rGt7UiuSEm2fJbdvatMDMjnoHcsmu2O8JPC-HFQiXh6ngRCK3sF2_4b4UyX0JVXhHpxjBQ8x5c8XsnIeTZ3aUTgbTgSyNyWUv8DRPErIgc8NPgHwUZJdDYLPSZR8okscCaNgwRB39Yylhg8at70Uhxm9nz7aL93IFgvsQ9MwWF7pEWNoUUIokVvVC_Tws3sG71K9H045LvwK7oUDsaqa0l7KQjNM_Pl4zxd5M9E8_vtGtSAIZuohSPDwF37BjrbKRHc0sRro5i85YSfAEirgSDPJXR2qDt_y6yU48oD0r_y3btFwJY-fWy3rEmQ8NkdnXH73LKwpSldBv56CQYIFz1E'
         };
         axios.get("http://localhost:8000/api/authors", { headers }).then((data) => {
             console.log(data);
@@ -19,15 +19,17 @@ function Author() {
 
     return (
         <div className="Author">
-             {post.map((item, i) => {
+            Auteurs
+            {post.map((data) => {
                 return (
-                    <div className="auteurs" key={i}>
-                        <p>{"auteur id: " + item.id}</p>
-                        <p>{"Prénom: " + item.firstName}</p>
-                        <p>{"Nom: " + item.lastName}</p>
-                        <p>{"Livre id: " + item.books.id + " / Titre: " + item.books.title + " / Quatrième: " + item.books.coverText}</p>
-                        
-                    </div>
+                    <ul className="auteurs" key={data.id}>
+                        <li>{"auteur id: " + data.id}</li>
+                        <li>{"Prénom: " + data.firstName}</li>
+                        <li>{"Nom: " + data.lastName}</li>
+                        <ul>{data.books.map(function(d,id){ return (<li key={id}>{"Livre id: " + d.id + " / Titre: " + d.title + " / Quatrième: " + d.coverText}</li>)})}
+                            
+                        </ul>
+                    </ul>
                 );
             })}
         </div>
